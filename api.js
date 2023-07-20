662063e4048afffb30fbff56c2f6dbecef638365
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-07-20 08:56:50 +0700

    updated2

--- api.js
+++ api.js
@@ -1,35 +1,32 @@
-const form = document.querySelector('form');
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
 
-form.addEventListener('submit', (elemen) => {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  elemen.preventDefault();
+  function PostSignUp(username, email, password) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  const formulir = new FormData(form);
-  
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+    const raw = JSON.stringify({
+      "username": username,
+      "email": email,
+      "password": password
+    });
 
-  var object = {};
-  formulir.forEach(function(value, key){
-      object;
-      object[key] = value;
-  });
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: JSON.stringify(object),
-    redirect: 'follow',
-  };
+    fetch("https://eoqw6d9d000sh04.m.pipedream.net", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
 
-  console.log(requestOptions);
-
-  fetch("https://eoqw6d9d000sh04.m.pipedream.net", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-  document.getElementById('form').reset();
-  })
-
-//upload
+  PostSignUp(username, email, password);
+})
